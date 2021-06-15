642850e4a4495482a04b9d1dafd5727985b65c7c
Dev1-Anand <dev1.anand.211@gmail.com>
2021-06-15 11:28:55 +0530

    Added test class

---
+++ src/test/java/com/sreejesh/MainApplicationClassTests.java
@@ -0,0 +1,16 @@
+package com.sreejesh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.test.context.SpringBootTest;
+
+//Comment added to test deploy phase
+@SpringBootTest
+public class MainApplicationClassTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
